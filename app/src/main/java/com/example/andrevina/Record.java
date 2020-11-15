cb4d1c7be26e280d8abf64a29a702a9b3e4681d1
imkrysis <elkrysss@gmail.com>
2020-11-15 06:33:40 +0100

    Another commit.

--- app/src/main/java/com/example/andrevina/Record.java
+++ app/src/main/java/com/example/andrevina/Record.java
@@ -6,55 +6,53 @@ import java.util.Comparator;
 
 public class Record implements Comparator<Record> {
 
-    int attempts;
     String nickname;
+    int attempts;
     int gameTime;
-    Bitmap photo;
+    Bitmap photoBitmap;
     String timeInfo;
 
-    Record(int attempts, String nickname, int gameTime, Bitmap photo, String timeInfo) {
+    Record(String nickname, int attempts, int gameTime, Bitmap photoBitmap, String timeInfo) {
 
-        this.attempts = attempts;
         this.nickname = nickname;
+        this.attempts = attempts;
         this.gameTime = gameTime;
-        this.photo = photo;
+        this.photoBitmap = photoBitmap;
         this.timeInfo = timeInfo;
 
-    }
-
-    public int getAttempts() {
-        return attempts;
     }
 
     public String getNickname() {
         return nickname;
     }
 
+    public int getAttempts() {
+        return attempts;
+    }
+
     public int getGameTime() {
         return gameTime;
     }
 
-    public Bitmap getPhoto() {
-        return photo;
+    public Bitmap getPhotoBitmap() {
+        return photoBitmap;
     }
 
     public String getTimeInfo() {
         return timeInfo;
     }
 
-    public int compare(Record a, Record b) {
+    public int compare(Record record1, Record record2) {
 
-        int attemptsComparison = a.getAttempts() - b.getAttempts();
+        int attemptsComparison = record1.getAttempts() - record2.getAttempts();
 
         if (attemptsComparison == 0) {
 
-            int timeComparison = a.getGameTime() - b.getGameTime();
+            int timeComparison = record1.getGameTime() - record2.getGameTime();
 
             if (timeComparison == 0) {
 
-                int nicknameComparison = a.getNickname().compareTo(b.getNickname());
-
-                return nicknameComparison;
+                return 0;
 
             } else {
 
